466d40f33f8228be0250b4155ab618e3bcb77120
unolife92 <unolife92@gmail.com>
2019-04-07 09:28:00 +0900

    lassot and SVM

--- class_sample_code/layers.py
+++ class_sample_code/layers.py
@@ -1,5 +1,5 @@
 import sys, os
-from lib.functions import sigmoid, softmax, cross_entropy_error, mean_squared_error
+from functions import sigmoid, softmax, cross_entropy_error, mean_squared_error
 import numpy as np
 
 class Dense:
@@ -27,4 +27,46 @@ class Dense:
 class SoftmaxWithLoss:
     def __init__(self):
         self.error = None
-        
+        self.y = None
+        self.t = None # true 값
+
+    def forward(self, x):
+        self.y = softmax(x)
+        return self.y
+
+    def loss(self, t):
+        self.t = t
+        self.error = cross_entropy_error(self.y, self.t)
+        return self.error
+
+    def backward(self, d_out = 1, learning_rate = None):
+        batch_size = self.t.shape[0]
+        d_x = (self.y - self.t) / batch_size
+        return d_x
+
+class Sigmoid:
+    def __init__(self):
+        self.y = None
+
+    def forward(self, x):
+        self.y = sigmoid(x)
+        return self.y
+
+    def backward(self, d_out, learning_rate = None):
+        return d_out * (1.0 - self.y) * self.y
+
+class Relu:
+    def __init__(self):
+        self.mask = None
+
+    def forward(self, x):
+        self.mask = (x <= 0)
+        out = x.copy()
+        out[self.mask] = 0
+        return out
+
+    def backward(self, d_out, learning_rate = None):
+        d_out[self.mask] = 0
+        d_x = d_out
+        return d_x
+    
